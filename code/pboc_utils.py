40cd1801d0cbfa5b32bd9d47d362e90316803d04
Muir Morrison <muir@caltech.edu>
2017-03-16 20:09:29 -0400

    Added several code files and updated pboc_utils.

--- code/pboc_utils.py
+++ code/pboc_utils.py
@@ -6,14 +6,19 @@ PRK. These functions were written by Griffin Chure and carry an MIT license.
 2017/03/10
 MJM: removed skimage requirement for CSHL PBoC
     (Canopy w/ Python3 lacks it)
+
+2017/03/15
+MJM: reinstated skimage, phase_segmentation, and extract_intensities, 
+    with slight variation from GC's original.
 """
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-# import skimage.io
-# import skimage.segmentation
-# import skimage.filters
-# import skimage.measure
+import skimage.io
+import skimage.segmentation
+import skimage.filters
+import skimage.measure
+import skimage.morphology
 import seaborn as sns
 
 
@@ -81,3 +86,44 @@ def bar3(data, xlabel='x', ylabel='y', zlabel='z', bin_step=1,
     ax.set_ylabel(ylabel)
     ax.set_zlabel(zlabel)
     return fig, ax
+
+def phase_segmentation(image, thresh, area_bounds=[1,3], ip_dist=0.16):
+    """Take a phase contrast image, segment by thresholding, and return 
+    the mask."""
+    # first rescale image to intensities from 0 to 1
+    im_float = (image - image.min()) / (image.max() - image.min())
+    # do background subtraction
+    im_blur = skimage.filters.gaussian(im_float, sigma=50.0)
+    im_sub = im_float - im_blur
+    # apply the threshold
+    im_thresh = im_sub < thresh
+    # next do area screen. but 1st need to label objects
+    im_lab = skimage.measure.label(im_thresh)
+    props = skimage.measure.regionprops(im_lab)
+    # initialize an empty image
+    approved_objects = np.zeros_like(im_lab)
+    # loop over object in labeled image and apply area screen
+    for labeled_obj in props:
+        # convert pixel area to physical area in um^2
+        area = labeled_obj.area * ip_dist**2
+        # apply area screen
+        if (area > area_bounds[0]) & (area < area_bounds[1]):
+            approved_objects += (im_lab == labeled_obj.label)
+    # clear border and relabel
+    im_border = skimage.segmentation.clear_border(approved_objects)
+    final_seg = skimage.measure.label(im_border)
+    return final_seg
+
+def extract_intensities(seg, fluo_im):
+    """Takes two images as args: 1st is a segmentation mask, 
+    2nd is a fluorescence image. Returns a list of intensities of each
+    object in fluorescence image."""
+    # get the regionprops of the fluorescent image, conditioned on the 
+    # segmentation mask
+    props = skimage.measure.regionprops(seg, intensity_image=fluo_im)
+    cell_ints = []
+    # loop over labeled objects
+    for labeled_obj in props:
+        # append intensity of each cell to list
+        cell_ints.append(labeled_obj.mean_intensity)
+    return np.array(cell_ints)
